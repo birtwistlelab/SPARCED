d6cb81d4b2c5759194ba5daef4a11796a71cacdd
ebenz99 <ebenz99@gmail.com>
2020-04-20 14:55:02 -0400

    directory copy

--- bin/modules/copyDir.py
+++ bin/modules/copyDir.py
@@ -3,9 +3,6 @@ from distutils.dir_util import copy_tree
 def copyDirectory(src, dest):
     try:
         copytree(src, dest)
-    except shutil.Error as e:
-        #directories are the same
-        print('Directory not copied. Error is %s' % e)
-    except OSError as e:
-        #invalid directory
-        print('Directory not copied. Error: %s' % e)
+    except e:
+        print('Directory not copied. Error')
+        exit(1)
