2ff1f74c6e7363d94a891451839cb44ab78d4d7d
ebenz99 <ebenz99@gmail.com>
2020-04-20 15:07:08 -0400

    proper name

--- bin/modules/copyDir.py
+++ bin/modules/copyDir.py
@@ -2,7 +2,7 @@ from distutils.dir_util import copy_tree
 
 def copyDirectory(src, dest):
     try:
-        copytree(src, dest)
-    except e:
+        copy_tree(src, dest)
+    except:
         print('Directory not copied. Error')
         exit(1)
