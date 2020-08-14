d46bb8bd99ca08b5de2917ae5a11820d9df375cb
Ethan Bensman <ebenz99@gmail.com>
2020-08-14 15:16:10 -0400

    not onverwriting output folder

--- bin/buildFolders.sh
+++ bin/buildFolders.sh
@@ -5,5 +5,5 @@ numItems=$(ls -dq sweep* | wc -l)
 for i in $( seq 0 $(( $numItems-1 )) )
     do
     rsync -avr --exclude="sweep*" --exclude="outputFolder*" "." "outputFolder$i"
-    cp "sweep$i.txt" "outputFolder$i"
+    mv "sweep$i.txt" "outputFolder$i/"
 done
