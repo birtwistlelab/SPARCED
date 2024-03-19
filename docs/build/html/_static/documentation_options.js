1216640b68c5622f257a5c32e9857d729cfeba20
chocolatecharlie <aamrit@clemson.edu>
2024-03-19 18:50:27 -0400

    Generate basic documentation with PDJ theme

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0',
+    VERSION: '00.00.01',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,26 +3,39 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- Path setup --------------------------------------------------------------
+import os
+import sys
+
+import sphinx_pdj_theme
+
+sys.path.insert(0, os.path.abspath('../SPARCED/model_creation/antimony_utils'))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'SPARCED'
 copyright = '2024, The Birtwistle Lab'
 author = 'The Birtwistle Lab'
-release = '0'
+release = '00.00.01'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode'
+]
+autosummary_generate = True
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_pdj_theme'
+html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
 html_static_path = ['_static']
