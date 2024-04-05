96a1af0eb2ee102a572a9da8b78723b97c085e36
chocolatecharlie <aamrit@clemson.edu>
2024-04-05 16:58:28 -0400

    Some work towards getting a similar SBML file than legacy - very close but not there yet

--- SPARCED/model_creation/utils/sbml_scripts/sbml_utils.py
+++ SPARCED/model_creation/utils/sbml_scripts/sbml_utils.py
@@ -5,7 +5,9 @@
 def write_compartments_annotations(f, comp):
     """Set compartments annotations using the last column of the compartments input file"""
     for row in comp:
-        f.getCompartments(row[0]).setAnnotation(row[2])
+        # For some unexplained reason setAnnotation throws an error
+        # Some investigation in the official documentation shows it should work...
+        f.getCompartment(row[0]).setAnnotation(row[2])
 
 def write_species_annotations(f, spec):
     """Set species annotations using the last column of the species input file"""
