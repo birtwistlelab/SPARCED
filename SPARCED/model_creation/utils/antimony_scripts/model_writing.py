fe6626001cb07e0e47f5cda74924ded06f9b91b8
chocolatecharlie <aamrit@clemson.edu>
2024-04-11 16:40:14 -0400

    Remove duplicate model name and add observables argument

--- SPARCED/model_creation/utils/antimony_scripts/model_writing.py
+++ SPARCED/model_creation/utils/antimony_scripts/model_writing.py
@@ -7,15 +7,14 @@ from model_creation.utils.arguments import parse_args
 from model_creation.utils.antimony_scripts.antimony_utils import *
 
 
-def antimony_write_model(antimony_model_name, output_dir_path, f_compartments,
+def antimony_write_model(model_name, output_dir_path, f_compartments,
                          f_stoichmatrix, f_output_parameters, f_ratelaws,
                          f_species, is_SPARCED):
     """
     Generate an Antimony file based on given data
-    Run this function alone using the "python -m" syntax outside the "utils"
-    subsubmodule
-    :param antimony_model_name: name for the generated antimony model
-    :type antimony_model_name: [str]
+    
+    :param model_name: name for the generated antimony model
+    :type model_name: [str]
     :param f_compartments: compartments & volumes file
     :type f_compartments: [str]
     :param f_stoichmat: stoichiometric matrix file
@@ -32,14 +31,14 @@ def antimony_write_model(antimony_model_name, output_dir_path, f_compartments,
     :rtype: ([str], list[str], list[str])
     
     """
-    antimony_file = output_dir_path + "ant_" + antimony_model_name + ".txt"
+    antimony_file = output_dir_path + "ant_" + model_name + ".txt"
 
     with open(antimony_file,"w") as antimony_model:
         # Write file's header
         if (is_SPARCED):
             antimony_model.write("# PanCancer Model by Birtwistle Lab\n")
         antimony_model.write("model {antimony}()\n\n"
-                            .format(antimony=antimony_model_name))
+                            .format(antimony=model_name))
         # Write compartments, species and reactions
         compartments, volumes, species = antimony_init(f_compartments,
                                                        f_species)
@@ -69,5 +68,5 @@ if __name__ == '__main__':
     f_ratelaws = args.inputdir + args.ratelaws
     f_species = args.inputdir + args.species
     # Write model
-    antimony_write_model(args.antimony, args.outputdir, f_compartments,
+    antimony_write_model(args.name, args.outputdir, f_compartments,
                          f_stoichmatrix, f_output_params, f_ratelaws, f_species)
--- SPARCED/model_creation/utils/arguments.py
+++ SPARCED/model_creation/utils/arguments.py
@@ -13,16 +13,16 @@ def parse_args():
     """
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-a', '--antimony',     default="SPARCED",
-                        help="desired name for the generated antimony model")
-    parser.add_argument('-b', '--sbml',         default="SPARCED",
-                        help="desired name for the generated SBML and AMICI models")
+    parser.add_argument('-b', '--observables',  default="Observables.txt",
+                        help="name of the observables file")
     parser.add_argument('-c', '--compartments', default="Compartments.txt",
                         help="name of the compartments file")
     parser.add_argument('-i', '--inputdir',     default="./data/core/",
                         help="relative path to the input directory")
     parser.add_argument('-m', '--stoichmatrix', default="StoicMat.txt",
                         help="name of the stoichiometric matrix file")
+    parser.add_argument('-n', '--name',         default="SPARCED",
+                        help="desired name for the generated antimony, SBML and AMICI models")
     parser.add_argument('-o', '--outputdir',    default="./models/SPARCED-standard/",
                         help="relative path to the output directory")
     parser.add_argument('-p', '--outputparams', default="out_Parameters.txt",
@@ -31,7 +31,7 @@ def parse_args():
                         help="name of the ratelaws file")
     parser.add_argument('-s', '--species',      default="Species.txt",
                         help="name of the species file")
-    parser.add_argument('-v', '--verbose',      action='store_true',
+    parser.add_argument('-v', '--verbose',      action='store_false',
                         help="display additional details during execution")
     parser.add_argument('-w', '--wild',         action='store_true',
                         help="running wild (without SPARCED hard-coded values/behaviors")
