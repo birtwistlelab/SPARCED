adb0168aa707f6b9c563a257f59bf10c4fc72637
chocolatecharlie <aamrit@clemson.edu>
2024-04-17 18:15:19 -0400

    Update documentation for SBML

--- SPARCED/compilation/utils/sbml_scripts/model_annotation.py
+++ SPARCED/compilation/utils/sbml_scripts/model_annotation.py
@@ -2,11 +2,24 @@
 # -*- coding: utf-8 -*-
 
 import libsbml
+import numpy as np
 
 from compilation.utils.sbml_scripts.sbml_utils import *
 
 
-def sbml_annotate_model(sbml_file_name, species, compartments):
+def sbml_annotate_model(sbml_file_name: str, species: np.ndarray,
+                        compartments: np.ndarray) -> None:
+    """Annotate species and compartments of the given SBML model
+
+    Arguments:
+        sbml_file_name: The SBML file path.
+        species: Content of the species input file.
+        compartments: Content of the compartments input file.
+
+    Returns:
+        Nothing.
+    """
+
     # Import SBML file
     sbml_reader = libsbml.SBMLReader()
     sbml_doc = sbml_reader.readSBML(sbml_file_name)
--- SPARCED/compilation/utils/sbml_scripts/sbml_utils.py
+++ SPARCED/compilation/utils/sbml_scripts/sbml_utils.py
@@ -2,16 +2,46 @@
 # -*- coding: utf-8 -*-
 
 
-def write_compartments_annotations(f, comp):
-    """Set compartments annotations using the last column of the compartments input file"""
-    for row in comp[1:]:
-        f.getCompartment(row[0]).setAnnotation(row[2])
+def write_compartments_annotations(f_sbml, compartments):
+    """Set compartments annotations in the given SBML file
 
-def write_species_annotations(f, spec):
-    """Set species annotations using the last column of the species input file"""
-    for i, row in enumerate(spec[1:]): # Skip header
-        annot = ""
+    Note:
+        The first row is considered as a header, and hence it is skipped.
+        Compartment names should be located on the first column of the array.
+        Annotations should be located on the third column of the array.
+
+    Arguments:
+        f_sbml: The loaded SBML file.
+        compartments: Content of the compartments input file.
+
+    Returns:
+        Nothing.
+    """
+
+    for row in compartments[1:]:
+        f_sbml.getCompartment(row[0]).setAnnotation(row[2])
+
+def write_species_annotations(f_sbml, species):
+    """Set species annotations in the given SBML file
+
+    Note:
+        The first row is considered as a header, and hence it is skipped.
+        Annotations should be located between the 4th and the last column of
+        the array.
+
+    Arguments:
+        f_sbml: The loaded SBML file.
+        species: Content of the species input file.
+
+    Returns:
+        Nothing.
+    """
+
+    for i, row in enumerate(species[1:]):
+        annotation = ""
         for col in range(4, len(row)):
-            aa = str(row[col].strip())
-            if not (aa == "nan" or aa == ""): annot += " " + row[col]
-        f.getSpecies(row[0]).setAnnotation(annot)
+            specie_annotation = str(row[col].strip())
+            if not (specie_annotation == "nan" or specie_annotation == ""):
+                annotation += " " + row[col]
+        f_sbml.getSpecies(row[0]).setAnnotation(annotation)
+
