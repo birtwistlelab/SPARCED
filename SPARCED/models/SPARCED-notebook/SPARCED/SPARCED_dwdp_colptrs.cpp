41dc62e60bfe3ab87b4124bb5b68903dada750e8
chocolatecharlie <aamrit@clemson.edu>
2024-04-18 15:02:17 -0400

    Add SPARCED notebook model

---
+++ SPARCED/models/SPARCED-notebook/SPARCED/SPARCED_dwdp_colptrs.cpp
@@ -0,0 +1,14 @@
+#include "amici/sundials_matrix_wrapper.h"
+#include "sundials/sundials_types.h"
+
+#include <array>
+#include <algorithm>
+
+namespace amici {
+namespace model_SPARCED {
+
+
+void dwdp_colptrs_SPARCED(SUNMatrixWrapper &dwdp){
+}
+} // namespace amici
+} // namespace model_SPARCED
