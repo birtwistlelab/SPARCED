2be66a8a2fc7512f83157ae73b77495988c8300b
chocolatecharlie <aamrit@clemson.edu>
2024-04-23 17:36:52 -0400

    Dirty hard-coding to test quickly the simulation pipeline

--- SPARCED/__main__.py
+++ SPARCED/__main__.py
@@ -8,6 +8,7 @@ from compilation.utils import *
 from compilation.create_model import *
 # Model simulation
 from simulation.ligands import *
+from simulation.run import *
 
 # First step is to build the model.
 # You may also want to comment this section out and run a model that was
@@ -19,10 +20,11 @@ launch_model_creation() # Process parsed arguments and launch model creation
 # You may want to load a custom extracellular concentration of ligands.
 # The basic function is usefull if you don't want to add any ligands or if you
 # need to adjust only EGF and/or insulin concentrations.
-ligands = basic_ligands()
+# TODO: implement reading arguments
+ligands = basic_ligands(1.0, 17.21) # WARNING: hard-coded values
 # If you need to adjust more species or simply if you're more comfortable
 # working with input files, use the load function instead.
 # ligands = load_input_ligands(your_file_path)
 
 # Run model
-# run_model(ligands)
+run_model(ligands)
