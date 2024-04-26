22691cfb8074fec791d6b3697fe029b5eaa867e9
chocolatecharlie <aamrit@clemson.edu>
2024-04-26 13:20:15 -0400

    Remove docker and kubernetes examples

--- examples/kubernetes/kube-runner/kube-clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Remove all pods that are not running.
-
-PODS=$(kubectl get pods --no-headers | grep 'Completed\|Error\|ContainerCannotRun' | awk '{ print $1 }')
-
-if [[ ! -z ${PODS} ]]; then
-	kubectl delete pods ${PODS}
-fi
