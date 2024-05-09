1413f6cfca78258baf3202172d76ced3ec4c10ef
ChocolateCharlie <aamrit@clemson.edu>
2024-05-09 17:48:03 -0400

    Create SPARCED-legacy model

---
+++ SPARCED/models/SPARCED_legacy/README.md
@@ -0,0 +1,14 @@
+# SPARCED legacy
+
+This model is the version 1.3.0 of SPARCED.
+
+Data consists of the original, unaltered input files.
+
+The model was generated from the Jupyter notebook located in the Demo folder of
+the version 1.3.0 of the main branch of the original SPARCED repository.
+
+Prefixes were manually added to the files and folder names to allow
+compatibility with current pipeline.
+
+The model name is "SPARCED".
+
---
+++ SPARCED/models/SPARCED_legacy/amici_SPARCED/compileMexFile.m
@@ -0,0 +1,9 @@
+% This compile script was automatically created from Python SBML import.
+% If mex compiler is set up within MATLAB, it can be run from MATLAB 
+% in order to compile a mex-file from the Python generated C++ files.
+
+modelName = 'SPARCED';
+amimodel.compileAndLinkModel(modelName, '', [], [], [], []);
+amimodel.generateMatlabWrapper(914, 102, 0, 2715, 0, 0, ...
+    [], ['simulate_' modelName '.m'], modelName, ...
+    'lin', 1, 1);
