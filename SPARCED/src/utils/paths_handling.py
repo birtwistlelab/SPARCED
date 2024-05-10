31b8cf969a4cfb60bfd84050a2733a9673d7bbe1
ChocolateCharlie <aamrit@clemson.edu>
2024-05-10 17:43:34 -0400

    Update append_subfolder() error handling

--- SPARCED/src/utils/paths_handling.py
+++ SPARCED/src/utils/paths_handling.py
@@ -3,38 +3,44 @@
 
 import os
 from pathlib import Path
+import sys
 
 
-def append_subfolder(folder: str | os.PathLike, subfolder: str) -> tuple[str | os.PathLike, bool]:
+def append_subfolder(folder: str | os.PathLike, subfolder: str,
+                     abort_on_error: bool=False) -> str | os.PathLike:
     """Append a subfolder to a folder path
 
     Arguments:
         folder: The folder path.
         subfolder: The subfolder name.
+        abort_on_error: Abort process when encountering an error.
 
     Returns:
-        A tuple composed of the subfolder's path and a boolean stating if an
-        error occured.
+        The subfolder's path.
     """
 
-    error_occured = False
     folder = Path(folder)
-    
+
     try:
         assert folder.exists()
     except:
-        print("WARNING: Folder doesn't exist.\nFolder name:{name}."
-              .format(name=folder))
-        error_occured = True
-    
+        print("WARNING: Folder doesn't exist. This is never normal.\nFolder \
+               name:{name}.".format(name=folder))
+        if abort_on_error:
+            print("Aborting now.")
+            sys.exit(0)
+
     subfolder_path = folder / subfolder
     
     try:
         assert subfolder_path.exists()
     except:
-        print("WARNING: Subfolder doesn't exist.\nSubfolder name: {name}."
-              .format(name=subfolder))
-        error_occured = True
+        print("WARNING: Subfolder doesn't exist. This is normal if you are
+               creating a new subfolder.\nSubfolder name: {name}."
+               .format(name=subfolder))
+        if abort_on_error:
+            print("Aborting now.")
+            sys.exit(0)
 
-    return(subfolder_path, error_occured)
+    return(subfolder_path)
 
