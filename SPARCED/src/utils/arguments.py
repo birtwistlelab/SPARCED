3eebd9fcb87188b7b0e2069a0923e10fc0d1432f
ChocolateCharlie <aamrit@clemson.edu>
2024-05-10 14:14:31 -0400

    Update arguments

--- SPARCED/src/utils/arguments.py
+++ SPARCED/src/utils/arguments.py
@@ -12,40 +12,49 @@ def parse_args():
     """
     
     parser = argparse.ArgumentParser()
-    # Capital letter => file & path names + flagD
-    parser.add_argument('-D', '--deterministic', action="store_false",
-                        help="don't run simulation in deterministic mode")
-    parser.add_argument('-I', '--inputdir',      default="./../data/core/",
-                        help="relative path to the core input directory (model creation)")
-    parser.add_argument('-J', '--inputsim',      default="./../data/simulation/",
-                        help="relative path to the simulation input directory")
-    parser.add_argument('-O', '--outputdir',     default="./../models/SPARCED-standard/",
-                        help="relative path to the output directory")
-    parser.add_argument('-P', '--outputparams',  default="out_Parameters.txt",
+    # Model creation
+    parser.add_argument('-i', '--input_data',           default="data/",
+                        help="name of the model subfolder containing the \
+                              SPARCED formated input files")
+    parser.add_argument('-o', '--output_parameters',    default="out_Parameters.txt",
                         help="desired name for the output parameters file")
-    parser.add_argument('-W', '--outputsim',    default="./../results/New-Simulation/",
-                        help="relative path to the directory where simulation \
-                              results will be saved")
-    # Lowcase letter => running options
-    parser.add_argument('-c', '--compound',      default=None,
-                        help="name of the compound (for example a drug) you wish to alter")
-    parser.add_argument('-d', '--dose',          default=0.0,
-                         help="concentration of the altered compound (nM)")
-    parser.add_argument('-n', '--name',          default="SPARCED",
-                        help="desired name for the generated model AND name of \
-                              the input model")
-    parser.add_argument('-s', '--simulation',    default="GrowthStim",
-                        help="desired name for the simulation output files")
-    parser.add_argument('-p', '--popsize',    default=1,
+    # Model creation & simulation
+    parser.add_argument('-m', '--model',                default="./../models/",
+                        help="relative path to the directory containing the \
+                              models folders")
+    parser.add_argument('n', '--name',                  default=None,
+                        help="name of the model\nModel creation: desired name \
+                              for the generated model (should be identical to \
+                              model's folder name).\nSimulation: name of the \
+                              input model.")
+    # Simulation
+    # -- Uppercase
+    parser.add_argument('-D', '--deterministic',        action="store_false",
+                        help="don't run simulation in deterministic mode")
+    parser.add_argument('-P', '--perturbations',        default=None,
+                        help="name of the perturbations file to use (will \
+                              override default)")
+    # -- Lowercase
+    parser.add_argument('-c', '--compound',             default=None,
+                        help="name of the compound (for example a drug) you \
+                              wish to adjust")
+    parser.add_argument('-d', '--dose',                 default=0.0,
+                        help="concentration of the specified compound (nM)")
+    parser.add_argument('-p', '--population_size',      default=1,
                         help="desired cell population size for the simulation")
-    parser.add_argument('-t', '--time',          default=1.0,
+    parser.add_argument('-r', '--results',              default="./../results/New-Simulation/",
+                        help="relative  path to the directory where simulation \
+                              results will be saved")
+    parser.add_argument('-s', '--simulation',           default="GrowthStim",
+                        help="desired name for the simulation output files")
+    parser.add_argument('-t', '--time',                 default=1.0,
                         help="desired virtual duration of the simulation (h)")
-    parser.add_argument('-v', '--verbose',       action='store_false',
+    parser.add_argument('-v', '--verbose',              action='store_false',
                         help="don't display additional details during execution")
-    parser.add_argument('-w', '--wild',          action='store_true',
+    parser.add_argument('-w', '--wild',                 action='store_true',
                         help="UNDER CONSTRUCTION\nrunning wild (without SPARCED \
                               hard-coded values/behaviors")
-    parser.add_argument('-x', '--exchange',      default=30,
+    parser.add_argument('-x', '--exchange',             default=30,
                          help="timeframe between modules information exchange \
                                during the simulation")
     return(parser.parse_args())
