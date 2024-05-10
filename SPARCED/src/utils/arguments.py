b6be2d672416b32f826940ea46bef73128af08a7
ChocolateCharlie <aamrit@clemson.edu>
2024-05-10 14:52:20 -0400

    Update arguments

--- SPARCED/src/utils/arguments.py
+++ SPARCED/src/utils/arguments.py
@@ -13,12 +13,12 @@ def parse_args():
     
     parser = argparse.ArgumentParser()
     # Compilation
-    parser.add_argument('-i', '--input_data',           default="data/",
-                        help="name of the model subfolder containing the \
-                              SPARCED formated input files")
     parser.add_argument('-o', '--output_parameters',    default="out_Parameters.txt",
                         help="desired name for the output parameters file")
     # Compilation & simulation
+    parser.add_argument('-i', '--input_data',           default="data/",
+                        help="name of the model subfolder containing the \
+                              SPARCED formated input files")
     parser.add_argument('-m', '--model',                default="./../models/",
                         help="relative path to the directory containing the \
                               models folders")
@@ -27,6 +27,14 @@ def parse_args():
                               the generated model (should be identical to \
                               model's folder name).\nSimulation: name of the \
                               input model.")
+    parser.add_argument('-v', '--verbose',              action='store_false',
+                        help="don't display additional details during execution")
+    parser.add_argument('-w', '--wild',                 action='store_true',
+                        help="UNDER CONSTRUCTION\nrunning wild (without SPARCED \
+                              hard-coded values/behaviors")
+    parser.add_argument('-y', '--yaml',                 default="config.yaml",
+                        help="name of the YAML file with all the input file names")
+
     # Simulation
     # -- Uppercase
     parser.add_argument('-D', '--deterministic',        action="store_false",
@@ -49,11 +57,6 @@ def parse_args():
                         help="desired name for the simulation output files")
     parser.add_argument('-t', '--time',                 default=1.0,
                         help="desired virtual duration of the simulation (h)")
-    parser.add_argument('-v', '--verbose',              action='store_false',
-                        help="don't display additional details during execution")
-    parser.add_argument('-w', '--wild',                 action='store_true',
-                        help="UNDER CONSTRUCTION\nrunning wild (without SPARCED \
-                              hard-coded values/behaviors")
     parser.add_argument('-x', '--exchange',             default=30,
                          help="timeframe between modules information exchange \
                                during the simulation")
