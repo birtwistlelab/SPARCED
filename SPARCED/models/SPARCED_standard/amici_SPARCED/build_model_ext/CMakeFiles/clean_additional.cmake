90023d7d13811d94251787d06db4d66cd626a351
ChocolateCharlie <aamrit@clemson.edu>
2024-05-10 19:15:03 -0400

    Recompile SPARCED standard

--- SPARCED/models/SPARCED_standard/amici_SPARCED/build_model_ext/CMakeFiles/clean_additional.cmake
+++
@@ -1,9 +0,0 @@
-# Additional clean files
-cmake_minimum_required(VERSION 3.16)
-
-if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Release")
-  file(REMOVE_RECURSE
-  "swig/CMakeFiles/_SPARCED.dir/SPARCEDPYTHON_wrap.cxx"
-  "swig/SPARCED.py"
-  )
-endif()
---
+++ SPARCED/models/SPARCED_standard/amici_SPARCED_standard/SPARCED_standard_x0_fixedParameters.cpp
@@ -0,0 +1,17 @@
+#include "amici/symbolic_functions.h"
+#include "amici/defines.h"
+#include "sundials/sundials_types.h"
+
+#include <array>
+
+#include "p.h"
+#include "k.h"
+
+namespace amici {
+namespace model_SPARCED_standard {
+
+void x0_fixedParameters_SPARCED_standard(realtype *x0_fixedParameters, const realtype t, const realtype *p, const realtype *k){
+}
+
+} // namespace amici
+} // namespace model_SPARCED_standard
