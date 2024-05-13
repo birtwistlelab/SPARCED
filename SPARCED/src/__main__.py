c9fea8ed067b5a53a5bb9367012f5c19b2282612
ChocolateCharlie <aamrit@clemson.edu>
2024-05-13 17:41:56 -0400

    Update main script

--- SPARCED/src/__main__.py
+++ SPARCED/src/__main__.py
@@ -6,8 +6,7 @@
 # Model compilation
 from compilation.launcher import launch_model_creation
 # Model simulation
-from simulation.utils.ligands import *
-from simulation.launchers import *
+from simulation.launchers import launch_experiment_simulation
 
 # First step is to build the model.
 # You may also want to comment this section out and run a model that was
@@ -15,15 +14,6 @@ from simulation.launchers import *
 # Note that building the model may take several minutes.
 launch_model_creation() # Process parsed arguments and launch model creation
 
-# Second step is to set the initial conditions.
-# You may want to load a custom extracellular concentration of ligands.
-# The basic function is usefull if you don't want to add any ligands or if you
-# need to adjust only EGF and/or insulin concentrations.
-ligands = basic_ligands_from_arguments()
-# If you need to adjust more species or simply if you're more comfortable
-# working with input files, use the load function instead.
-# ligands = load_input_ligands(your_file_path)
-
 # Finally you can run an experiment, i.e. one or several cell simulations
 # within the given initial conditions.
-launch_experiment_simulation(ligands) # Process parsed arguments and launch experiment
+launch_experiment_simulation() # Process parsed arguments and launch experiment
