46566ed1efb962c3d9b09067755c6ce0e3ee1222
ChocolateCharlie <aamrit@clemson.edu>
2024-05-13 16:52:52 -0400

    Update simulation scripts

--- SPARCED/src/simulation/launchers.py
+++ SPARCED/src/simulation/launchers.py
@@ -1,39 +1,72 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import sys
 
 import numpy as np
 
 from utils.arguments import parse_args
+from utils.data_handling import *
+from utils.sanitize import *
 from simulation.run_model import run_experiment
 
 
-def launch_experiment_simulation(ligands: np.ndarray) -> None:
+def launch_experiment_simulation(perturbations: np.ndarray) -> None:
+    """Launch experiment simulation function
+
+    Small routine to process the parsed arguments and call run_model()
+
+    Arguments:
+        perturbations:  The array specifying initial conditions perturbations.
+
+    Returns:
+        Nothing.
     """
-    Small routine to process parsed arguments and call run_model
-    """
+
     args = parse_args()
-    # Process arguments
-    is_SPARCED = not args.wild # if it's not wild then it's SPARCED
-    f_species = args.inputdir + args.species
-    f_genereg = args.inputsim + args.genereg
-    f_omics = args.inputsim + args.omics
-    model_path = args.outputdir + "amici_" + args.name
-    sbml_model = args.outputdir + "sbml_" + args.name + ".xml"
-    try:
-        assert args.popsize > 0
-    except:
-        print("{model_name}-{simulation_name}: Cell population size should be \
-               superior to zero (0). Current population size is: {size}.\n \
-               Aborting now.".format(model_name=args.name,
-                                     simulation_name=args.simulation,
-                                     size = args.popsize))
-        sys.exit(0)
-    run_experiment(args.name, model_path, args.simulation, args.outputsim, sbml_model,
-                   args.deterministic, args.exchange, args.popsize,
-                   args.time, f_species, ligands, f_genereg, f_omics,
-                   args.verbose, is_SPARCED, args.compound, args.dose)
+    # Model
+    model_name = sanitize_model_name(args.name)
+    model_path = append_subfolder(args.model, model_name, True)
+    amici_name = "amici_" + model_name
+    amici_path = append_subfolder(args.model, amici_name, True)
+    sbml_name = "sbml_" + model_name + ".xml"
+    sbml_path = append.subfolder(args.model, sbml_name, True)
+    # Input data files
+    input_files = load_simulation_input_files(model_path, args.input_data, args.yaml)
+    # Population size
+    popsize = sanitize_popsize(args.popsize)
+    # Runtime booleans
+    is_SPARCED = not args.wild  # if it's not wild then it's SPARCED
+    verbose = args.verbose
+    run_experiment(model_name, args.simulation, args.results, amici_path,
+                   sbml_path, input_files, perturbations, args.deterministic,
+                   args.popsize, args.time, args.exchange, args.verbose, is_SPARCED)
+
+def load_simulation_input_files(model_path: str | os.PathLike, data_folder: str,
+                                yaml_name: stre) -> dict[str, str | os.PathLike]:
+    """Load simulation input data files paths dictionnary
+
+    Arguments:
+        model_path: The model folder path.
+        data_folder: The input data files folder name.
+        yaml_name: The YAML configuration file name.
+
+    Returns:
+        A dictionnary containing all the input data file paths.
+    """
+
+    input_files_configuration = load_input_data_config(model_path, data_folder,
+                                                       yaml_name)
+    simulation_files = input_files_configuration["simulation"]
+    # Load root of simulation input data files
+    simulation_root = simulation_files.pop("root", None)
+    simulation_data_path = append_subfolder(data_path, simulation_root, True)
+    # Reconstruct full path for each input file listed in the dictionnary
+    for input_file in simulation_files.keys():
+        if input_file != "perturbations":
+            simulation_files[input_file] = append_subfolder(simulation_data_path, simulation_files[input_file])
+    return(simulation_files)
 
 
 if __name__ == '__main__':
--- SPARCED/src/simulation/run_model.py
+++ SPARCED/src/simulation/run_model.py
@@ -12,19 +12,36 @@ from simulation.modules.RunSPARCED import RunSPARCED
 from simulation.utils.initial_conditions import load_species_initial_conditions
 from simulation.utils.output import save_simulation_output
 
-# TODO: why to we have to pass again initial concentrations to the function
-# when those are already in the SBML file?
-# We should just pass the modified ligands and compound concentrations and
-# leave the rest as is.
 
-def run_experiment(model_name: str, model_path: str, simulation_name: str,
-                   output_sim: str, sbml_model: str, is_deterministic: bool,
-                   exchange: int, popsize: int, duration: float,
-                   f_species: str, ligands: np.ndarray, f_genereg: str,
-                   f_omics: str, verbose: bool, is_SPARCED: bool,
-                   compound: str=None, dose: str=None) -> None:
-    """
-    Run an experiment (one or several cells with the same initial conditions)
+def run_experiment(model_name: str, simulation_name: str,
+                   output_sim: str | os.PathLike, model_path: str | os.PathLike,
+                   sbml_model: str | os.PathLike,
+                   input_files: dict[str, str | os.PathLike],
+                   is_deterministic: bool, popsize: int, duration: float,
+                   exchange: int, verbose: bool, is_SPARCED: bool,
+                   perturbation: np.ndarray=None) -> None:
+    """Run an experiment
+
+    Run an experiment consisting of one or several cells within the same
+    initial conditions.
+
+    Arguments:
+        model_name: The name of the model.
+        simulation_name: The desired name for the simulation results.
+        output_sim: The path where to store the results.
+        model_path: The path to the AMICI model folder.
+        sbml_model: The path to the SBML model.
+        input_files: A dictionnary containing simualation input files paths.
+        is_deterministic: Run on deterministic mode.
+        popsize: The cell population size in the experiment.
+        duration: The duration of the experiment.
+        exchange: The timeframe between module information exchange.
+        verbose: Verbose.
+        is_SPARCED: Use SPARCED hard-coded values/behaviors.
+        perturbations: An array containing changes to make to initial conditions.
+
+    Return:
+        Nothing.
     """
     # Load model
     sys.path.insert(0, os.path.abspath(model_path)) # TODO: fix the import on the next line to avoid messing up with paths
@@ -32,18 +49,19 @@ def run_experiment(model_name: str, model_path: str, simulation_name: str,
     model = model_module.getModel()
     if verbose:
         print("{model_name}: Successfully loaded {module}\n"
-               .format(model_name=model_name, module=model_module))
+               .format(model_name=model_name, module=model_module)) 
     # Set time points
     model.setTimepoints(np.linspace(0, exchange, 2))
     # Compute initial conditions
-    species_initial_conditions = load_species_initial_conditions(f_species, ligands, compound, dose)
+    species_initial_conditions = load_species_initial_conditions(perturbations)
     # Run experiment
     cell_number = 0
     while cell_number < int(popsize):
         run_single_simulation(model, simulation_name, output_sim, cell_number, sbml_model,
                               is_deterministic, exchange, duration,
-                              species_initial_conditions, verbose, f_genereg, f_omics)
-        cell_number += 1  
+                              species_initial_conditions, verbose,
+                              input_files["genereg"], input_files["omics"])
+        cell_number += 1
 
 def run_single_simulation(model, simulation_name: str, output_sim: str, simulation_number: int,
                           sbml_model: str, is_deterministic: bool,
