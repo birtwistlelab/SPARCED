be866acc978fa192b844f6f17fb3b16d6801d51a
ChocolateCharlie <aamrit@clemson.edu>
2024-05-15 15:21:50 -0400

    Fix bug in population check

--- SPARCED/src/utils/sanitize.py
+++ SPARCED/src/utils/sanitize.py
@@ -39,16 +39,16 @@ def sanitize_popsize(popsize: int, abort_on_error: bool=False) -> int:
     """
 
     try:
-        assert popsize > 0
+        assert int(popsize) > 0
     except:
         print("ERROR: Cell population size should be superior to zero (0). \
                 Current population size is: {size}."
-              .format(size = args.popsize))
+              .format(size = popsize))
         if abort_on_error:
             print("Aborting now.")
             sys.exit(0)
         else:
             print("Setting automatically population size to one (1).")
             popsize = 1
-    return(popsize)
+    return(int(popsize))
 
