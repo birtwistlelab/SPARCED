1289ec3bbd9f3afec40d10d4dc96f145d294b0f7
chocolatecharlie <aamrit@clemson.edu>
2024-05-22 17:26:32 -0400

    Remove old IFNg model

--- SPARCED/models/SPARCED-IFNg/amici_SPARCED_IFNg/compileMexFile.m
+++
@@ -1,9 +0,0 @@
-% This compile script was automatically created from Python SBML import.
-% If mex compiler is set up within MATLAB, it can be run from MATLAB 
-% in order to compile a mex-file from the Python generated C++ files.
-
-modelName = 'SPARCED_IFNg';
-amimodel.compileAndLinkModel(modelName, '', [], [], [], []);
-amimodel.generateMatlabWrapper(954, 958, 2829, 0, 0, 0, ...
-    [], ['simulate_' modelName '.m'], modelName, ...
-    'lin', 1, 1);
